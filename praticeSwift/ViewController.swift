3c392dbb41f9f5b2f360978033ae8943df074ce3
dnzhelloo <dnzhelloo@icloud.com>
2023-07-01 14:43:24 +0300

    Initial Commit

---
+++ praticeSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  praticeSwift
+//
+//  Created by Deniz on 1.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
